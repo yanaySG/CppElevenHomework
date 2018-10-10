0769f6578bb11459e7c759f0f04263b5731b8183
Yanay Rolo <yanay.sg@gmail.com>
2018-10-10 14:53:11 +0200

    Adding simple and fast messages and fixing minor bugs

--- main.cpp
+++ main.cpp
@@ -20,39 +20,34 @@ double* gen_rand(int N)
 /** MAIN HERE ******************************/
 int main(int argc, char *argv[])
 {
-   auto start = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
 
    
-   int N=2;
+   int N=1000;
    if (argc > 1)
       N=std::stoi(argv[1]);
+
+   std::cout << "Creating matrixes A,B and C of size N = " << N << std::endl << std::endl;
    
    Matrix A = Matrix(N,N,gen_rand(N*N));
    Matrix B = Matrix(N,N,gen_rand(N*N));
    Matrix C = Matrix(N,N,gen_rand(N*N));
-   
-   //std::cout << "Matrix A" << std::endl;
-   //A.print();
-   //std::cout << "Matrix B" << std::endl;
-   //B.print();
-   //std::cout << "Matrix C" << std::endl;
-   //C.print();
    
    auto start1 = std::chrono::high_resolution_clock::now();
 
    Matrix D = A*B+C;
   
    auto end1 = std::chrono::high_resolution_clock::now();
-   std::chrono::duration<double> diff1 = end1-start1;
-   std::cout << "Expression calculation time: " << diff1.count() << "s." << std::endl;
-   
-   std::cout << "Diag of Matrix D = A*B+C" << std::endl;
-   std::cout << D.diag() << std::endl;
+
+   std::cout << "Calculated matrix D = A*B+C." << N << std::endl;
   
-  
+   std::cout << "Diagonal of matrix D = " << D.diag() << std::endl<< std::endl;
    auto end = std::chrono::high_resolution_clock::now();
+   
+   std::chrono::duration<double> diff1 = end1-start1;
    std::chrono::duration<double> diff = end-start;
-   std::cout << "Total time: " << diff.count() << "s." << std::endl;
+   std::cout << "(calculation time: " << diff1.count() << "s.)" << std::endl;
+   std::cout << "(total time: " << diff.count() << "s.)" << std::endl;
 
    return 0;
 }
